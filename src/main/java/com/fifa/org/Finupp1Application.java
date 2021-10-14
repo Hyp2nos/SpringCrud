b5945ccf39563596cc808c906ffad66560103014
fifaliana <andryfifa279@gmail.com>
2021-10-14 13:18:03 +0300

    Premier commit

---
+++ src/main/java/com/fifa/org/Finupp1Application.java
@@ -0,0 +1,13 @@
+package com.fifa.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finupp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finupp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fifa/org/Finupp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifa.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finupp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
