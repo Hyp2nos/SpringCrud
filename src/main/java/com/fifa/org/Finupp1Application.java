1ecaa87a44590f478d8989023d4e1ac952fbb162
fifaliana <andryfifa279@gmail.com>
2021-10-14 13:20:26 +0300

    Deuxieme commit

--- src/main/java/com/fifa/org/Finupp1Application.java
+++ src/main/java/com/fifa/org/Finupp1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Finupp1Application {
 
 	public static void main(String[] args) {
+		System.out.println("2 eme commit");
 		SpringApplication.run(Finupp1Application.class, args);
 	}
 
